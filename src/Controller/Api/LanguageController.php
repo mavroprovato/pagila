7854310d940d5c868aa7df93366d043600801c31
Kostas Kokkoros <mavroprovato@gmail.com>
2025-03-20 03:51:20 +0200

    Paginate the languages controller

--- src/Controller/Api/LanguageController.php
+++ src/Controller/Api/LanguageController.php
@@ -5,28 +5,36 @@ declare(strict_types=1);
 namespace App\Controller\Api;
 
 use App\Entity\Language;
-use App\Repository\LanguageRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Attribute\Route;
 
 /**
  * The language controller
  */
 #[Route('/api/languages')]
-class LanguageController extends AbstractController
+class LanguageController extends BaseController
 {
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getEntityClass(): string
+    {
+        return Language::class;
+    }
 
     /**
      * List languages.
      *
-     * @param LanguageRepository $repository The language repository.
+     * @param int $page The page to fetch.
+     * @param int $perPage The number of results to fetch per page.
      * @return Response The response.
      */
     #[Route('/', name: 'languages_list')]
-    public function list(LanguageRepository $repository): Response
+    public function list(#[MapQueryParameter] int $page = 1, #[MapQueryParameter] int $perPage = 100): Response
     {
-        return $this->json($repository->findAll());
+        return parent::list($page, $perPage);
     }
 
     /**
