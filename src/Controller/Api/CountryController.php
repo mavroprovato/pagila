10980df31507f841b8f94615b1d46188bdb9041b
Kostas Kokkoros <mavroprovato@gmail.com>
2025-03-20 02:32:48 +0200

    Remove unused imports

--- src/Controller/Api/CountryController.php
+++ src/Controller/Api/CountryController.php
@@ -5,9 +5,6 @@ declare(strict_types=1);
 namespace App\Controller\Api;
 
 use App\Entity\Country;
-use App\Model\PaginatedResponse;
-use App\Repository\CountryRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Attribute\Route;
